d7bd0e086dbea459b93a6b2ad4627344e0771aba
GreatWayne8 <seanwayne3355@gmail.com>
2023-11-21 11:14:07 +0300

    cart and checkout

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -2,12 +2,17 @@ from .models import Cart, CartItem
 from carts.views import _cart_id
 
 def counter(request):
+    print(request)
     cart_count = 0
     if 'admin' in request.path:
         return {}
     else:
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
+            if request.user.is_authenticated:
+                cart_items=CartItem.objects.all().filter(user=request.user)
+            else:
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
             cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
