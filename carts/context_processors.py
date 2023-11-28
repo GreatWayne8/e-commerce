f35c4cc2617a90fc74af95e27f64441f48d311e9
GreatWayne8 <seanwayne3355@gmail.com>
2023-11-28 12:39:25 +0300

    order and payments

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,8 +1,8 @@
 from .models import Cart, CartItem
-from carts.views import _cart_id
+from .views import _cart_id
+
 
 def counter(request):
-    print(request)
     cart_count = 0
     if 'admin' in request.path:
         return {}
@@ -10,10 +10,9 @@ def counter(request):
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
             if request.user.is_authenticated:
-                cart_items=CartItem.objects.all().filter(user=request.user)
+                cart_items = CartItem.objects.all().filter(user=request.user)
             else:
                 cart_items = CartItem.objects.all().filter(cart=cart[:1])
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
         except Cart.DoesNotExist:
