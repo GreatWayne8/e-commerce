94c2a617410ad0e08505cf518331e9c9a874b9fe
GreatWayne8 <seanwayne3355@gmail.com>
2023-12-04 12:24:06 +0300

    reviews and ratings

--- orders/models.py
+++ orders/models.py
@@ -3,18 +3,16 @@ from accounts.models import Account
 from store.models import Product, Variation
 
 
-
+# Create your models here.
 class Payment(models.Model):
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    payment_id = models.CharField(max_length=100)
-    payment_method = models.CharField(max_length=100)
-    amount_paid = models.CharField(max_length=100) # this is the total amount paid
+    user =models.ForeignKey(Account,on_delete=models.CASCADE)
+    Payment_id =models.CharField(max_length=100)
+    amount_paid= models.CharField(max_length=100) 
     status = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.payment_id
-
+        return self.Payment_id 
 
 class Order(models.Model):
     STATUS = (
@@ -24,10 +22,10 @@ class Order(models.Model):
         ('Cancelled', 'Cancelled'),
     )
 
-    user = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
-    payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(Account,on_delete=models.SET_NULL, null=True)
+    Payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
     order_number = models.CharField(max_length=20)
-    first_name = models.CharField(max_length=50)
+    first_name =models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=15)
     email = models.EmailField(max_length=50)
@@ -48,18 +46,17 @@ class Order(models.Model):
 
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
-
+    
     def full_address(self):
-        return f'{self.address_line_1} {self.address_line_2}'
+        return f'{self.full_address_1} {self.full_address_2}'
 
     def __str__(self):
         return self.first_name
 
-
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account,on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variations = models.ManyToManyField(Variation, blank=True)
     quantity = models.IntegerField()
@@ -68,5 +65,6 @@ class OrderProduct(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
     def __str__(self):
-        return self.product.product_name
+        return self.user.first_name
